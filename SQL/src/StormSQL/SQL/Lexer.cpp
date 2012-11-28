50f6da0005282ff777e5e636ceedbdade5086645
Georgy Angelov <georgyangelov@gmail.com>
2012-11-29 00:55:31 +0200

    Lexer class to help the (not yet started) SQL parser To be tested

---
+++ SQL/src/StormSQL/SQL/Lexer.cpp
@@ -0,0 +1,137 @@
+#include "Lexer.h"
+#include <algorithm>
+#include <sstream>
+
+namespace StormSQL
+{
+	namespace SQL
+	{
+		/* Private */
+		void Lexer::ignoreWhitespace()
+		{
+			char c = in->peek();
+			while (c == ' ' || c == '\t' || c == '\n')
+			{
+				in->get();
+				c = in->peek();
+			}
+		}
+
+		Token Lexer::getToken(const string& str, TokenType type) const
+		{
+			Token res;
+
+			if (type == TokenType::IntValue)
+			{
+				// Parse string to int
+				stringstream strStream(str);
+				strStream >> res.longIntData;
+			}
+
+			res.strData = str;
+			res.type = type;
+
+			return res;
+		}
+		
+		inline bool Lexer::isLetter(char c) const
+		{
+			return c >= 'a' && c <= 'z' || c >= 'A' && c <= 'Z'; 
+		}
+
+		inline bool Lexer::isNumber(char c) const
+		{
+			return c >= '0' && c <= '9';
+		}
+
+		void Lexer::toLower(string& str) const
+		{
+			std::transform(str.begin(), str.end(), str.begin(), ::tolower);
+		}
+
+		/* Public */
+		Lexer::Lexer(istream& _in)
+		{
+			in = &_in;
+		}
+
+		Token Lexer::NextToken()
+		{
+			ignoreWhitespace();
+
+			char c = in->peek();
+			string tmp;
+
+			if (c == '(' || c == ')')
+				return getToken(&c, TokenType::Parenthesis);
+			else if (c == ',')
+				return getToken(&c, TokenType::Separator);
+			else if (c == '`')
+			{
+				while ((c = in->get()) != '`')
+					tmp += c;
+
+				return getToken(tmp, TokenType::Identifier);
+			}
+			else if (c == '\'' || c == '\"')
+			{
+				char endP = c;
+				char last = endP;
+
+				while ((c = in->get()) != endP && last != '\\')
+				{
+					tmp += c;
+					last = c;
+				}
+
+				return getToken(tmp, TokenType::StringValue);
+			}
+			else if (isNumber(c))
+			{
+				while (isNumber(c))
+				{
+					tmp += c;
+					c = in->get();
+				}
+
+				return getToken(tmp, TokenType::IntValue);
+			}
+			else if (isLetter(c))
+			{
+				while (isLetter(c))
+				{
+					tmp += c;
+					c = in->get();
+				}
+
+				return getToken(tmp, TokenType::StringValue);
+			}
+			else
+			{
+				tmp += c;
+
+				switch (c)
+				{
+				case '-':
+				case '+':
+				case '*':
+				case '/':
+					return getToken(tmp, TokenType::Operator);
+
+				case '=':
+				case '!':
+				case '<':
+				case '>':
+
+					if (in->peek() == '=')
+						tmp += c;
+
+					return getToken(tmp, TokenType::Operator);
+
+				default:
+					throw UnknownTokenException(tmp);
+				}
+			}
+		}
+	}
+}
---
+++ SQL/src/StormSQL/SQL/Lexer.h
@@ -0,0 +1,69 @@
+#ifndef _H_LEXER_INCLUDED
+#define _H_LEXER_INCLUDED
+
+#include <string>
+
+using namespace std;
+
+namespace StormSQL
+{
+	namespace SQL
+	{
+		enum TokenType
+		{
+			// 'SELECT', 'INSERT', 'INTO', 'FROM', 'VALUES', ...
+			Keyword,
+			// table names, database names, field names
+			Identifier,
+			// ','
+			Separator,
+			// '(' and ')'
+			Parenthesis,
+			// ==, !=, !, +, -, /, *, <, >, <=, >=
+			Operator,
+			StringValue,
+			IntValue
+		};
+
+		struct Token
+		{
+			TokenType type;
+
+			string strData;
+			long longIntData;
+		};
+
+		class Lexer
+		{
+		private:
+			istream* in;
+
+			// Helper functions
+			void ignoreWhitespace();
+			//Token getToken(const string&) const;
+			Token getToken(const string&, TokenType type) const;
+			long toLong(const string&) const;
+
+			bool isLetter(char c) const;
+			bool isNumber(char c) const;
+			void toLower(string& c) const;
+
+		public:
+			Lexer(istream&);
+
+			Token NextToken();
+		};
+
+		class UnknownTokenException
+			: public runtime_error
+		{
+		public:
+			UnknownTokenException(string token)
+				: runtime_error("Unknown token" + token)
+			{
+			}
+		};
+	}
+}
+
+#endif
